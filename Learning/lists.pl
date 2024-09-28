7c5acb782195100e6e2847dbb364c308743cd1a4
Aditya Patel <adityapatel@adityas-air.home>
2024-09-28 11:00:25 -0400

    feat: minor changes

--- Learning/lists.pl
+++ Learning/lists.pl
@@ -1,30 +1,47 @@
-members(X, [X|T]).
-members(X, [Y|T]) :- not X = Y, members(X, T).
+memberlist([Item|_T], Item).
+memberlist([H|T], Item) :- not H = Item, memberlist(T, Item).
 
-multiple(X, [X|T]).
-multiple(X, [Y|T]) :- multiple(X, T).
 
-appendlist([], L, L).
+multiplemember([Item|_T], Item).
+multiplemember([H|T], Item) :- multiplemember(T, Item).
+
+
 appendlist(L, [], L).
-appendlist([H1|T1], L2, [H1|T3]) :- appendlist(T1, L2, T3).
+appendlist([], L, L).
+appendlist([H|T], L2, [H|Tail]) :- appendlist(T, L2, Tail).
 
-last(X, [X|[]]).
-last(X, [H|X]).
-last(X, [H|T]) :- last(X, T).
+
+lastmember(Item, [Item|[]]).
+lastmember(Item, [H|T]) :- lastmember(Item, T).
+
 
 prefix([], L).
-prefix([H1|T1], [H2|T2]) :- H1 = H2, prefix(T1, T2).
+prefix([H|T1], [H|T2]) :- prefix(T1, T2).
+
+
+replaceFirst(X, Y, [], []).
+replaceFirst(X, Y, [X|T], [Y|T]).
+replaceFirst(X, Y, [H|T1], [H|T2]) :- replaceFirst(X, Y, T1, T2).
+
 
 replaceAll(X, Y, [], []).
 replaceAll(X, Y, [X|T1], [Y|T2]) :- replaceAll(X, Y, T1, T2).
-replaceAll(X, Y, [H|T1], [H|T2]) :- replaceAll(X, Y, T1, T2).
+replaceAll(X, Y, [H|T1], [H|T2]) :- not H = X, replaceAll(X, Y, T1, T2).
 
-replaceFirst(X, Y, [], []).
-replaceFirst(X, Y, [X|T1], [Y|T1]).
-replaceFirst(X, Y, [H|T1], [H|T2]) :- not X = H, replaceFirst(X, Y, T1, T2).
 
-sumlist([], 0).
-sumlist([H|T], Sum) :- sumlist(T, NewSum), Sum is NewSum + H.
+sumRecursion([], 0).
+sumRecursion([H|T], S) :- sumRecursion(T, SubS), S is SubS + H.
 
-lengthlist([], 0).
-lengthlist([_H|T], Length) :- lengthlist(T, NewLength), Length is NewLength + 1.
+
+sumCounter(L, S) :- sumAccumulator(L, 0, S).
+sumAccumulator([], Acc, Acc).
+sumAccumulator([H|T], Acc, S) :- NewAcc is Acc + H, sumAccumulator(T, NewAcc, S).
+
+
+lengthRecursion([], 0).
+lengthRecursion([H|T], L) :- lengthRecursion(T, SubL), L is SubL + 1.
+
+
+lengthCounter(L, Length) :- lengthAccumulator(L, 0, Length).
+lengthAccumulator([], Acc, Acc).
+lengthAccumulator([H|T], Acc, Length) :- NewAcc is Acc + 1, lengthAccumulator(T, NewAcc, Length).
