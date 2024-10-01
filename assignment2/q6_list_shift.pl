8998988b0821b4bfd1d9d057fd749e43a959b68a
Aditya Patel <adityapatel@Adityas-MacBook-Air.local>
2024-10-01 11:34:53 -0400

    feat: solved question 6

--- assignment2/q6_list_shift.pl
+++ assignment2/q6_list_shift.pl
@@ -14,3 +14,28 @@
 
 %%%%% SECTION: listShift
 %%%%% Put your rules for listShift and any helper predicates below
+
+listShift(List, 0, List).
+
+listShift(List, ShiftLength, List) :-
+    listLength(List, Length),
+    ShiftLength >= Length.
+
+listShift(next(H, T), ShiftLength, Result) :-
+    ShiftLength > 0,
+    listLength(next(H, T), Length),
+    not ShiftLength >= Length,
+    SubShiftLength is ShiftLength - 1,
+    appendRight(T, H, SubList),
+    listShift(SubList, SubShiftLength, Result).
+
+appendRight(nil, Element, next(Element, nil)).
+
+appendRight(next(Head, T1), Element, next(Head, T2)) :-
+    appendRight(T1, Element, T2).
+
+listLength(nil, 0).
+
+listLength(next(_H, T), L) :-
+    listLength(T, SubL),
+    L is SubL + 1.
