7e47b097fb22d92f28f5a2a4a20c74a2817a15bd
Osanyem Osadebe <osanyemo@gmail.com>
2024-10-19 16:28:15 -0400

    add assignment 3 folder

---
+++ assignment3/q3_fertilizers.pl
@@ -0,0 +1,43 @@
+% Enter the names of your group members below.
+% If you only have 2 group members, leave the last space blank
+%
+%%%%%
+%%%%% NAME: 
+%%%%% NAME:
+%%%%% NAME:
+%
+% Add the required rules in the corresponding sections. 
+% If you put the rules in the wrong sections, you will lose marks.
+%
+% You may add additional comments as you choose but DO NOT MODIFY the comment lines below
+%
+
+%%%%% SECTION: minAndMaxList
+%%%%% Below, you should define rules for the predicate "minList(List, M)", 
+%%%%% which takes in an input List and finds the minimal element there.
+%%%%%
+%%%%% You should also define rules for the predicate "maxList(List, M)", 
+%%%%% which takes in an input List and finds the maximal element there.
+%%%%%
+%%%%% If you introduce any other helper predicates for implementing minList
+%%%%% and maxList, they should be included in this section.
+
+
+
+%%%%% SECTION: fertilizersSolve
+%%%%% Below, you should define rules for the predicate "solve", which takes in your list of
+%%%%% variables and finds an assignment for each variable which solves the problem.
+%%%%%
+%%%%% You should also define rules for the predicate "solve_and_print" which calls your
+%%%%% solve predicate and prints out the results in an easy to read, human readable format.
+%%%%% The predicate "solve_and_print" should take in no arguments
+%%%%%
+%%%%% This section should also include your domain definitions and any other helper
+%%%%% predicates (other than minList, maxList, and their helpers) that you choose to introduce.
+
+
+solve_and_print :-
+
+
+
+
